3f4acb6026bc9ae2f903db69b66fb2971fba9266
Bitbyte <Bitbyte@DESKTOP-1ENPTBA>
2021-05-12 22:23:06 +0200

    First commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,203 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool isGrounded;
+    public bool isSprinting;
+
+    private Transform cam;
+    private World world;
+
+    public float walkSpeed = 3f;
+    public float sprintSpeed = 6f;
+    public float jumpForce = 5f;
+    public float gravity = -9.8f;
+
+    public float playerWidth = 0.15f;
+    public float playerHeight = 2f;
+
+    private float horizontal;
+    private float vertical;
+    private float mouseHorizontal;
+    private float mouseVertical;
+    private Vector3 velocity;
+    private float verticalMomentum = 0;
+    private bool jumpRequest;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        cam = GameObject.Find("Main Camera").transform;
+        world = GameObject.Find("World").GetComponent<World>();
+    }
+
+    private void FixedUpdate()
+    {       
+        CalculateVelocity();
+
+        if (jumpRequest)
+        {
+            Jump();
+        }
+
+                 
+        transform.Translate(velocity, Space.World);
+    }
+
+    private void Update()
+    {
+        GetPlayerInputs();
+
+        transform.Rotate(Vector3.up * mouseHorizontal);
+        cam.Rotate(Vector3.right * -mouseVertical);
+    }
+
+    void Jump()
+    {
+        verticalMomentum = jumpForce;
+        isGrounded = false;
+        jumpRequest = false;
+    }
+
+    private void CalculateVelocity()
+    {
+        //Apply gravity
+        if (verticalMomentum > gravity)
+            verticalMomentum += Time.fixedDeltaTime * gravity;
+
+        if (isSprinting && isGrounded)
+            velocity = ((transform.forward * vertical) + (transform.right * horizontal)) * Time.fixedDeltaTime * sprintSpeed;
+        else
+            velocity = ((transform.forward * vertical) + (transform.right * horizontal)) * Time.fixedDeltaTime * walkSpeed;
+
+
+        
+        velocity += Vector3.up * verticalMomentum * Time.fixedDeltaTime;
+
+
+
+        if ((velocity.z > 0 && front) || (velocity.z < 0 && back))
+            velocity.z = 0;
+        if ((velocity.x > 0 && right) || (velocity.x < 0 && left))
+            velocity.x = 0;
+        
+
+        if (velocity.y < 0)
+            velocity.y = CheckDownSpeed(velocity.y);
+        else if (velocity.y > 0)
+            velocity.y = CheckUpSpeed(velocity.y);
+
+    }
+
+    private void GetPlayerInputs()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        mouseHorizontal = Input.GetAxis("Mouse X");
+        mouseVertical = Input.GetAxis("Mouse Y");
+
+        if (Input.GetButtonDown("Sprint"))
+            isSprinting = true;
+        if (Input.GetButtonUp("Sprint"))
+            isSprinting = false;
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+            jumpRequest = true;
+    }
+
+    private float CheckDownSpeed(float downSpeed)
+    {
+        if (
+            world.CheckForVoxel(new Vector3(transform.position.x - playerWidth, transform.position.y + downSpeed, transform.position.z - playerWidth)) ||
+            world.CheckForVoxel(new Vector3(transform.position.x + playerWidth, transform.position.y + downSpeed, transform.position.z - playerWidth)) ||
+            world.CheckForVoxel(new Vector3(transform.position.x + playerWidth, transform.position.y + downSpeed, transform.position.z + playerWidth)) ||
+            world.CheckForVoxel(new Vector3(transform.position.x - playerWidth, transform.position.y + downSpeed, transform.position.z + playerWidth))
+            )
+        {
+            isGrounded = true;
+            return 0;
+        }
+        else
+        {
+            isGrounded = false;
+            return downSpeed;
+        }
+
+    }
+    private float CheckUpSpeed (float upSpeed)
+    {
+        if (
+            world.CheckForVoxel(new Vector3(transform.position.x - playerWidth, transform.position.y + playerHeight + upSpeed, transform.position.z - playerWidth)) ||
+            world.CheckForVoxel(new Vector3(transform.position.x + playerWidth, transform.position.y + playerHeight + upSpeed, transform.position.z - playerWidth)) ||
+            world.CheckForVoxel(new Vector3(transform.position.x + playerWidth, transform.position.y + playerHeight + upSpeed, transform.position.z + playerWidth)) ||
+            world.CheckForVoxel(new Vector3(transform.position.x - playerWidth, transform.position.y + playerHeight + upSpeed, transform.position.z + playerWidth))
+            )
+        {
+            return 0;
+        }
+        else
+        {
+            return upSpeed;
+        }
+    }
+
+    public bool front
+    {
+        get
+        {
+            if (
+                world.CheckForVoxel(new Vector3(transform.position.x, transform.position.y, transform.position.z + playerWidth)) ||
+                world.CheckForVoxel(new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z + playerWidth))
+                )
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public bool back
+    {
+        get
+        {
+            if (
+                world.CheckForVoxel(new Vector3(transform.position.x, transform.position.y, transform.position.z - playerWidth)) ||
+                world.CheckForVoxel(new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z - playerWidth))
+                )
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public bool left
+    {
+        get
+        {
+            if (
+                world.CheckForVoxel(new Vector3(transform.position.x - playerWidth, transform.position.y, transform.position.z)) ||
+                world.CheckForVoxel(new Vector3(transform.position.x - playerWidth, transform.position.y + 1f, transform.position.z + playerWidth))
+                )
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public bool right
+    {
+        get
+        {
+            if (
+                world.CheckForVoxel(new Vector3(transform.position.x + playerWidth, transform.position.y, transform.position.z)) ||
+                world.CheckForVoxel(new Vector3(transform.position.x + playerWidth, transform.position.y + 1f, transform.position.z + playerWidth))
+                )
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Assets/Scripts/VoxelData.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class VoxelData
+{
+    public static readonly int ChunkWidth = 16;
+    public static readonly int ChunkHeight = 128;
+    public static readonly int WorldSizeInChunks = 100;
+
+    public static int WorldSizeInVoxels
+    {
+        get { return WorldSizeInChunks * ChunkWidth; }
+    }
+
+    public static readonly int ViewDistanceInChunks = 8;
+
+    public static readonly int TextureAtlasSizeInBlocks = 4;
+    public static float NormalizedBlockTextureSize
+    {
+        get { return 1f / (float)TextureAtlasSizeInBlocks; }
+    }
+
+    public static readonly Vector3[] voxelVerts = new Vector3[8]
+    {
+        new Vector3(0.0f, 0.0f, 0.0f),
+        new Vector3(1.0f, 0.0f, 0.0f),
+        new Vector3(1.0f, 1.0f, 0.0f),
+        new Vector3(0.0f, 1.0f, 0.0f),
+        new Vector3(0.0f, 0.0f, 1.0f),
+        new Vector3(1.0f, 0.0f, 1.0f),
+        new Vector3(1.0f, 1.0f, 1.0f),
+        new Vector3(0.0f, 1.0f, 1.0f)
+    };
+
+    public static readonly Vector3[] faceChecks = new Vector3[6]
+    {
+        new Vector3(0.0f, 0.0f, -1.0f),  // Back Face
+        new Vector3(0.0f, 0.0f, 1.0f),   // Front Face
+        new Vector3(0.0f, 1.0f, 0.0f),   // Top Face
+        new Vector3(0.0f, -1.0f, 0.0f),  // Bottom Face
+        new Vector3(-1.0f, 0.0f, 0.0f),  // Left Face
+        new Vector3(1.0f, 0.0f, 0.0f)    // Right Face
+    };
+
+    public static readonly int[,] voxelTris = new int[6, 4] // Commented out vertice data that's not needed.
+    {
+        // 0 -> 1 -> 2    then 2 -> 1 -> 3
+
+        {0, 3, 1, /*1, 3,*/ 2 }, // Back Face
+        {5, 6, 4, /*4, 6,*/ 7 }, // Front Face
+        {3, 7, 2, /*2 ,7,*/ 6 }, // Top Face
+        {1, 5, 0, /*0, 5,*/ 4 }, // Bottom Face
+        {4, 7, 0, /*0, 7,*/ 3 }, // Left Face
+        {1, 2, 5, /*5, 2,*/ 6 }  // Right Face
+    };
+
+    public static readonly Vector2[] voxelUVs = new Vector2[4] // Commented out extra UV's we dont need
+    {
+        new Vector2(0.0f, 0.0f),   // Back Face
+        new Vector2(0.0f, 1.0f),   // Front Face
+        new Vector2(1.0f, 0.0f),   // Top Face
+        //new Vector2(1.0f, 0.0f), // Bottom Face
+        //new Vector2(0.0f, 1.0f), // Left Face
+        new Vector2(1.0f, 1.0f),   // Right Face
+    };
+}
